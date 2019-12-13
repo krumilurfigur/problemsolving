0fbf8e89a8a16b2fe858c6f7afaa0377824a1f00
krumilurfigur <gabrieloberg3@gmail.com>
2019-12-13 11:18:03 +0100

    lektionen 12/13

--- src/adventofcode1.java
+++ src/adventofcode1.java
@@ -1,7 +1,32 @@
+import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray;
+
 import java.io.*;
 import java.util.Scanner;
 
 public class adventofcode1 {
+    public static void main(String[] args) {
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("./src/test.txt"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            in = new Scanner(System.in);
+        }
+        int[] opcode;
+         int n = 0;
+        double sum = 0;
+
+        while (in.hasNextLine()) {
+            opcode[n] = Integer.parseInt(in.nextLine());
+            n++;
+        }
+        System.out.println(opcode[1]);
+    }
+}
+
+
+class day1part2 {
     public static void main(String[] args) {
         Scanner in = null;
         try {
@@ -18,9 +43,30 @@ public class adventofcode1 {
             m = Double.parseDouble(in.nextLine());
             m = m/3;
             m = Math.floor(m);
-            m =- 2;
+            m -= 2;
             sum += m;
         }
         System.out.println(sum);
+    }
+}
+
+class day2part1 {
+    public static void main(String[] args) {
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("./src/test.txt"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            in = new Scanner(System.in);
+        }
+        int[] opcode = new int[];
+        int n = 0;
+
+        while (in.hasNextLine()) {
+            opcode[n] = Integer.parseInt(in.nextLine());
+            n++;
+        }
+        System.out.println(opcode[0]);
     }
 }
